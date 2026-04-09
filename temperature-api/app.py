397a3677c24338915c4a99faf5225b2af788c3b2
KIProkopenko <pok.nok@yandex.ru>
2026-04-09 14:39:01 +0700

    feat: temperature-api + docker-compose + Postman collection

--- temperature-api/app.py
+++ temperature-api/app.py
@@ -9,6 +9,13 @@ def get_temperature(location: str = None):
     temperature = round(random.uniform(-20.0, 40.0), 2)
     return {"location": location, "temperature": temperature}
 
+@app.get("/sensors")
+def get_sensors():
+    return [
+        {"id": 1, "name": "Thermostat 1", "location": "Living Room"},
+        {"id": 2, "name": "Outdoor Sensor", "location": "Garden"}
+        ]
+
 @app.get("/")
 def root():
     return {"message": "Temperature API is running"}
